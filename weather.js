595a0cc669dbb2e0a7f9a15b4c131bae4b49ebb8
Harthikpandiya <123759272+Harthikpandiya@users.noreply.github.com>
2025-11-01 17:34:08 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -59,8 +59,10 @@ app.get("/weather/:city", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🌤 Server running on port ${PORT}`));
+
+
 
 
 
